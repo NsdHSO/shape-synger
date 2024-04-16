a1372ad74bf8e6880ab1262176b37c6a218e148b
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-04-16 05:44:35 +0300

    feat: add possibility to use app in eslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,10 @@
-import { inject, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import {
-  TranslateLoader,
-  TranslateModule,
-  TranslateService
-} from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
