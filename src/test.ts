83bb2194aca443ff9aa9c1ff627fb3186f78e174
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-04-17 07:35:20 +0300

    test: add config to not repeat every each case boiler plate

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,38 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
+
+import { signal } from '@angular/core';
+
+import { NakedPipe } from './app/core/pipes/naked.pipe';
+import { CoreService } from './app/core/core.service';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
 );
+// Setup function to configure TestBed with necessary providers and imports
+function configureTestingModule() {
+  TestBed.configureTestingModule({
+    imports: [NakedPipe],
+    providers: [
+      {
+        provide: CoreService,
+        useValue: {
+          baseApp: signal('ion'),
+        },
+      },
+    ],
+  });
+}
+
+// Call the setup function before each test case
+beforeEach(() => {
+  configureTestingModule();
+});
