b51952479700115f216c6dec2e2582987b0545a1
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-04-20 16:36:24 +0300

    feat: remove key form env and take from process prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,17 @@
+import { writeFile } from 'fs';
+
 export const environment = {
   production: true,
   apiOverViewUrl: 'http://localhost:3000/overview',
-  supabaseUrl: 'https://icqyhfpzjnxnacyqegtg.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljcXloZnB6am54bmFjeXFlZ3RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM0Mzc1NDMsImV4cCI6MjAyOTAxMzU0M30.7LyoJroYUcJmzRhsRxtjLsNs-rJAbClpwP8wAO5xX5I',
+  supabaseUrl: ${process.env.supabaseUrl},
+  supabaseKey: ${process.enb.supabaseKey}
 };
+
+// Generate environment.ts file
+writeFile('./src/environments/environment.ts', environment, function (err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(`Angular environment.ts file generated`);
+  }
+});
