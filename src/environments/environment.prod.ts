f57816520bd552ab74acbe43c05d1a8587cf8994
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-04-20 16:55:12 +0300

    feat: add package.lock and set-env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,3 @@
-import { writeFile } from 'fs';
-
 export const environment = {
   production: true,
-  apiOverViewUrl: 'http://localhost:3000/overview',
-  supabaseUrl: ${process.env.supabaseUrl},
-  supabaseKey: ${process.enb.supabaseKey}
 };
-
-// Generate environment.ts file
-writeFile('./src/environments/environment.ts', environment, function (err) {
-  if (err) {
-    throw console.error(err);
-  } else {
-    console.log(`Angular environment.ts file generated`);
-  }
-});
