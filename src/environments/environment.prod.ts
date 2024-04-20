1b2a7e384ab2871c90b325ecd2b0a3970a4e961e
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-04-20 16:05:28 +0300

    feat: add supabase config for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true,
+  apiOverViewUrl: 'http://localhost:3000/overview',
+  supabaseUrl: 'https://icqyhfpzjnxnacyqegtg.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljcXloZnB6am54bmFjeXFlZ3RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM0Mzc1NDMsImV4cCI6MjAyOTAxMzU0M30.7LyoJroYUcJmzRhsRxtjLsNs-rJAbClpwP8wAO5xX5I',
 };
