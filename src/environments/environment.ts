aebf73fa28a9484468bc4489862e914f934f4900
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-04-25 08:08:27 +0300

    chore:🌻 add env in gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiOverViewUrl: 'http://localhost:3000/overview',
+  supabaseUrl: 'https://icqyhfpzjnxnacyqegtg.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljcXloZnB6am54bmFjeXFlZ3RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM0Mzc1NDMsImV4cCI6MjAyOTAxMzU0M30.7LyoJroYUcJmzRhsRxtjLsNs-rJAbClpwP8wAO5xX5I',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
