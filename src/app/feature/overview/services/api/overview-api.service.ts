d520d1fe5cf5c005fb346ac83329636c05223493
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-04-26 12:36:31 +0300

    chore:🌻 add new berer token

--- src/app/feature/overview/services/api/overview-api.service.ts
+++ src/app/feature/overview/services/api/overview-api.service.ts
@@ -14,7 +14,7 @@ export class OverviewApiService {
   private readonly overViewToken = inject(API_URL);
   headers = new Headers({
     'Content-Type': 'application/json',
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsImVtYWlsIjoiaWFuY3V0cnVtcEBnbWFpbC5jb20iLCJpYXQiOjE3MTQwMTc3OTksImV4cCI6MTcxNDEwNDE5OX0.6n-l1oWUaM8CiHjFYXH2-C8mLXHNPO1r9HJHWHRgGps`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsImVtYWlsIjoiaWFuY3V0cnVtcEBnbWFpbC5jb20iLCJpYXQiOjE3MTQxMTgzNTcsImV4cCI6MTcxNDIwNDc1N30.AMdmeZOjwCarArk5ENWRozMLvKokW5h3PhZxjSjaH2M`,
   }) as any;
   getCards = this.httpClient.get(this.overViewToken + '', {
     headers: this.headers,
